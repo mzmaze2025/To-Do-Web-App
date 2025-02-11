fffa207b12ba6c0973c766928cbb5070901abf2f
Marti <m.makhandi@gmail.com>
2025-02-11 17:43:23 +0100

    Make Todos Completion Work

--- web.py
+++ web.py
@@ -10,7 +10,8 @@ def add_todo():
 
 st.title("My Todo App")
 st.subheader("This is my todo app")
-st.write("This app is to increase your productivity. By ticking the todo, it will be set as completed.")
+st.write("This app is to increase your productivity and help you save time. "
+         "By ticking the todo, it will be set as completed.")
 
 for index, todo in enumerate(todos):
     checkbox = st.checkbox(todo, key = todo)
