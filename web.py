1eca7b2040b80828e20c41bcd8bcdc79b5de9440
Marti <m.makhandi@gmail.com>
2025-02-11 17:23:22 +0100

    Minor modifications

--- web.py
+++ web.py
@@ -18,7 +18,7 @@ for index, todo in enumerate(todos):
         todos.pop(index)
         functions.write_todos(todos)
         del st.session_state[todo]
-        st.experimental_rerun()
+        st.rerun()
 
 st.text_input("", placeholder= "Add a todo...", on_change=add_todo, key="new_todo")
 
